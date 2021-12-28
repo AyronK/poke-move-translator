8db31986901c4e772c508028681015a94a94c395
Marcin Kotlicki <kotlickimarcin@gmail.com>
2021-12-28 19:52:04 +0100

    Languages list + local storage cache

--- src/Poke.MoveTranslator.PWA/Program.cs
+++ src/Poke.MoveTranslator.PWA/Program.cs
@@ -1,7 +1,10 @@
+using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Poke.MoveTranslator.PWA;
 using MudBlazor.Services;
+using Poke.MoveTranslator.PWA.Services;
+using PokeApiNet;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -9,5 +12,13 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddMudServices();
+
+builder.Services.AddBlazoredLocalStorage();
+
+builder.Services.AddScoped<PokeApiClient>();
+
+builder.Services.AddScoped<IPokeApiService, PokeApiServiceCachedByLocalStorage>((c) => 
+    new PokeApiServiceCachedByLocalStorage(new PokeApiService(c.GetRequiredService<PokeApiClient>()), 
+        c.GetRequiredService<ILocalStorageService>()));
 
 await builder.Build().RunAsync();
---
+++ src/Poke.MoveTranslator.PWA/Services/PokeApiService.cs
@@ -0,0 +1,65 @@
+using Blazored.LocalStorage;
+using PokeApiNet;
+
+namespace Poke.MoveTranslator.PWA.Services;
+
+public class PokeApiService : IPokeApiService, IDisposable
+{
+    private PokeApiClient PokeApi { get; }
+
+    public PokeApiService(PokeApiClient pokeApi)
+    {
+        PokeApi = pokeApi;
+    }
+
+    public async Task<string[]> GetLanguages()
+    {
+        NamedApiResourceList<Language> result = await PokeApi.GetNamedResourcePageAsync<Language>(100, 0);
+        return result.Results.Select(l => l.Name).ToArray();
+    }
+
+    public void Dispose()
+    {
+        PokeApi.Dispose();
+    }
+}
+
+public class PokeApiServiceCachedByLocalStorage : IPokeApiService
+{
+    private const string LocalStorageKeyPrefix = "Poke.API";
+    
+    private IPokeApiService ApiService { get; }
+    private ILocalStorageService LocalStorage { get; }
+
+    public PokeApiServiceCachedByLocalStorage(IPokeApiService apiService, ILocalStorageService localStorage)
+    {
+        ApiService = apiService;
+        LocalStorage = localStorage;
+    }
+
+    public async Task<string[]> GetLanguages()
+    {
+        return await LocalStorage.GetOrAddAsync(LocalStorageKeyPrefix + "/languages", ApiService.GetLanguages);
+    }
+}
+
+public static class LocalStorageServiceExtensions
+{
+    public static async Task<T> GetOrAddAsync<T>(this ILocalStorageService localStorageService, string key, Func<Task<T>> valueFactory)
+    {
+        bool exists = await localStorageService.ContainKeyAsync(key);
+
+        if (!exists)
+        {
+            T value = await valueFactory();
+            await localStorageService.SetItemAsync(key, value);
+        }
+
+        return await localStorageService.GetItemAsync<T>(key);
+    }
+}
+
+public interface IPokeApiService
+{
+    Task<string[]> GetLanguages();
+}
